9596a63683bf6f8c20c576d9599227556b90061d
james bishop <bishbashbosh51@hotmail.com>
2021-04-12 18:05:14 +0100

    added responsive nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Router } from "@angular/router";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    isCollapsed = true;
     constructor(public ngAuthService: NgAuthService) { }
   title = 'gamer-zone';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,11 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
-
 import { NgAuthService } from "./ng-auth.service";
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AdminPageComponent } from './components/admin-page/admin-page.component';
-
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 @NgModule({
     declarations: [
@@ -30,17 +29,30 @@ import { AdminPageComponent } from './components/admin-page/admin-page.component
       VerifyEmailComponent,
       HomeComponent,
       ProfileComponent,
-      AdminPageComponent
+      AdminPageComponent,
+      
+      
     ],
     imports: [
       BrowserModule,
       AppRoutingModule,
       AngularFireModule.initializeApp(environment.firebase),
       AngularFireAuthModule,
-      AngularFirestoreModule,    
+      AngularFirestoreModule,
+      HighlightModule,  
+       
     ],
-    providers: [NgAuthService],
-    bootstrap: [AppComponent]
+    providers: [
+       {
+        provide: NgAuthService
+       }, 
+    
+    ],
+        
+    bootstrap: [AppComponent],
+    
+    
+    
   })
   export class AppModule { }
   
