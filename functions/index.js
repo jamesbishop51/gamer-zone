2337e7bb08d29c956c8bcd3a0f2d4a6b0e1d2e67
james bishop <bishbashbosh51@hotmail.com>
2021-04-12 13:16:40 +0100

    added the firebase functions for admin roles

---
+++ functions/index.js
@@ -0,0 +1,35 @@
+const functions = require('firebase-functions');
+const admin=require("firebase-admin");
+admin.initializeApp();
+
+
+exports.addAdminRole = functions.https.onCall((data) => {
+
+    // add custom claim to user
+    // setCustomerClaims is provided by firebase SDK functionality
+    return admin.auth().getUserByEmail(data.email).then(user =>{
+        return admin.auth().setCustomUserClaims(user.uid, {
+            admin:true
+        })
+    }).then(() => {
+        return{
+            message: `${data.email} setup admin`
+        }
+    }).catch(err => {
+        return err;
+    })
+})
+
+exports.removeAdminRole = functions.https.onCall((data) => {
+
+    // add custom claim to user
+    return admin.auth().getUserByEmail(data.email).then(user =>{
+        return admin.auth().setCustomUserClaims(user.uid, null)
+    }).then(() => {
+        return{
+            message: `${data.email} removed as admin`
+        }
+    }).catch(err => {
+        return err;
+    });
+});
