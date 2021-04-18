70a35fc503424989d48c7fca20c8e697eb9dbba8
james bishop <bishbashbosh51@hotmail.com>
2021-04-18 21:42:47 +0100

    added docker/heroku hosting and updated rawg api

--- src/app/services/rawg-api.service.ts
+++ src/app/services/rawg-api.service.ts
@@ -11,13 +11,13 @@ import { GameCollection } from 'src/app/interfaces/game'
 export class RawgApiService {
 
     private readonly searchGameUrl 
-
+private readonly newUrl
   constructor( private _http: HttpClient ) { 
     
-    this.searchGameUrl = 'https://api.rawg.io/api/games?page_size=1&search=';
-
+    //this.searchGameUrl = 'https://api.rawg.io/api/key=games?key=1bd066df00ce4f4981b34685b13901c8&page_size=1&search='; they decided to add a key at the wrong time!!!!!
+    this.newUrl ='https://api.rawg.io/api/games?key=1bd066df00ce4f4981b34685b13901c8&page_size=5&search=' //new search with key
   }
   public getGamesFromString(name: string): Observable<GameCollection> {
-    return this._http.get<GameCollection>(this.searchGameUrl + name );
+    return this._http.get<GameCollection>(this.newUrl + name );
   }
 }
